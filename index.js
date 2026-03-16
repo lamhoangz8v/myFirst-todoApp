280fc802a14d430c97a99a43f90bd5e9bccbf213
Lam Hoang <lamhoangz8v@gmail.com>
2026-03-16 19:30:14 +0700

    create database

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ const path = require("path");
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// THÊM ĐOẠN NÀY ĐỂ TỰ ĐỘNG TẠO BẢNG
+pool
+  .query(
+    `
+  CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY,
+    task_name VARCHAR(255) NOT NULL,
+    is_completed BOOLEAN DEFAULT FALSE
+  );
+`,
+  )
+  .then(() => {
+    console.log("Bảng todos đã sẵn sàng hoặc đã được tạo thành công!");
+  })
+  .catch((err) => {
+    console.error("Lỗi khi tạo bảng:", err);
+  });
+
 app.get("/todos", async (req, res) => {
   try {
     const allTodos = await pool.query("SELECT * FROM todos");
@@ -62,6 +84,8 @@ app.put("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Sever đang chạy tại port: http://localhost:3000/todos");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server đang chạy tại port: ${PORT}`);
 });
